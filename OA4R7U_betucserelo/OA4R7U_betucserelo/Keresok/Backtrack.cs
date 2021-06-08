d1925f3bb0c7c90a68e982f3142689b463e12857
Konya Donat <konya.donat@gmail.com>
2021-06-08 16:25:21 +0200

    Backtrack created

---
+++ OA4R7U_betucserelo/OA4R7U_betucserelo/Keresok/Backtrack.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA4R7U_betucserelo
+{
+    class Backtrack : Kereso
+    {
+        public Backtrack()
+        {
+            Kereses();
+        }
+        public override void Kereses()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
