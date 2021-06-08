d8f48e2b5c692b297c2c85013dcd94e047cbcf41
Konya Donat <konya.donat@gmail.com>
2021-06-08 13:57:55 +0200

    Csomopont finished

--- OA4R7U_betucserelo/OA4R7U_betucserelo/Allapotter/Csomopont.cs
+++ OA4R7U_betucserelo/OA4R7U_betucserelo/Allapotter/Csomopont.cs
@@ -8,5 +8,37 @@ namespace OA4R7U_betucserelo
 {
     class Csomopont
     {
+        Allapot allapot;
+        Csomopont szulo;
+        int koltseg;
+        int operatorIndex;
+
+        public Csomopont(Allapot allapot, int operatorIndex)
+        {
+            this.allapot = allapot;
+            this.operatorIndex = operatorIndex;
+        }
+
+        public Csomopont(Allapot allapot, Csomopont szulo)
+        {
+            this.allapot = allapot;
+            this.szulo = szulo;
+
+            if (szulo == null)
+                koltseg = 0;
+            else
+                this.koltseg = this.szulo.koltseg + 1;
+        }
+
+        public int Koltseg { get => koltseg; set => koltseg = value; }
+        public int OperatorIndex { get => operatorIndex; set => operatorIndex = value; }
+        internal Allapot Allapot { get => allapot; set => allapot = value; }
+        internal Csomopont Szulo { get => szulo; set => szulo = value; }
+
+        public override bool Equals(object obj)
+        {
+            Csomopont masik = (Csomopont)obj;
+            return Allapot.Equals(masik.Allapot);
+        }
     }
 }
