0006b7ff11aeaa23cc957297645c115c819e7283
Konya Donat <konya.donat@gmail.com>
2021-06-08 13:29:15 +0200

    Project was created

---
+++ OA4R7U_betucserelo/OA4R7U_betucserelo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA4R7U_betucserelo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
