cbd9a887972a567901fb8a329b09feaedf267411
Konya Donat <konya.donat@gmail.com>
2021-06-08 16:34:04 +0200

    Backtrack finished

--- OA4R7U_betucserelo/OA4R7U_betucserelo/Keresok/Backtrack.cs
+++ OA4R7U_betucserelo/OA4R7U_betucserelo/Keresok/Backtrack.cs
@@ -14,7 +14,31 @@ namespace OA4R7U_betucserelo
         }
         public override void Kereses()
         {
-            throw new NotImplementedException();
+            Stack<Csomopont> ut = new Stack<Csomopont>();
+            ut.Push(new Csomopont(new Allapot(), 0));
+
+            while (ut.Count > 0 && !ut.Peek().Allapot.celfeltel())
+            {
+                Csomopont aktual = ut.Peek();
+                if (aktual.OperatorIndex < Operatorok.Count)
+                {
+                    Operator aktualop = Operatorok[aktual.OperatorIndex];
+                    if (aktualop.Elofeltetel(aktual.Allapot))
+                    {
+                        Allapot uj = aktualop.atir(aktual.Allapot);
+                        Csomopont ujcsomo = new Csomopont(uj, 0);
+                        if (!ut.Contains(ujcsomo))
+                            ut.Push(ujcsomo);
+                    }
+                    aktual.OperatorIndex++;
+                }
+                else
+                    ut.Pop();
+            }
+            foreach (Csomopont csomopont in ut)
+                Ut.Add(csomopont.Allapot);
+            Ut.Reverse();
         }
+        
     }
 }
