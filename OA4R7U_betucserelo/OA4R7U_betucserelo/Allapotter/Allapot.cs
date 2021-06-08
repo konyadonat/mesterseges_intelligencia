8a227f19bc0ac847f6d7187b5899bbdb49cb181c
Konya Donat <konya.donat@gmail.com>
2021-06-08 13:37:11 +0200

    Allapot finished

--- OA4R7U_betucserelo/OA4R7U_betucserelo/Allapotter/Allapot.cs
+++ OA4R7U_betucserelo/OA4R7U_betucserelo/Allapotter/Allapot.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,45 @@ namespace OA4R7U_betucserelo
 {
     class Allapot
     {
+        public static string path = @"C:\Users\konya\Documents\GitHub\mesterseges_intelligencia\OA4R7U_betucserelo\OA4R7U_betucserelo\wordlist.txt";
+        public string[] szavak = File.ReadAllLines(path);
 
+        public static string kezdoszo = "mouse";
+        private char[] karakterek = new char[5];
 
+        public char[] Karakterek { get => karakterek; set => karakterek = value; }
+        public static string vegszo = "tiger";
 
+        public Allapot()
+        {
+            for (int i = 0; i < kezdoszo.Length; i++)
+            {
+                karakterek.Append(kezdoszo[i]);
+            }
+        }
+
+        public bool celfeltel()
+        {
+            for (int i = 0; i < vegszo.Length; i++)
+            {
+                if (!(karakterek[i] == vegszo[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public override bool Equals(object obj)
+        {
+            Allapot masikallapot = (Allapot)obj;
+            for (int i = 0; i < 5; i++)
+            {
+                if (karakterek[i] != masikallapot.karakterek[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
