1dc110f0b517274ea6838bc4e5999d9ab3ee7d06
Konya Donat <konya.donat@gmail.com>
2021-06-08 15:42:01 +0200

    Main configured

--- OA4R7U_betucserelo/OA4R7U_betucserelo/Program.cs
+++ OA4R7U_betucserelo/OA4R7U_betucserelo/Program.cs
@@ -10,6 +10,32 @@ namespace OA4R7U_betucserelo
     {
         static void Main(string[] args)
         {
+            string beirt;
+            do
+            {
+                Console.WriteLine("Adott kereső használatához írja be a kereső kezdőbetűjét!");
+                Console.WriteLine("Mélységi-m");
+                beirt = Console.ReadLine();
+
+                if (beirt == "m")
+                    Kiir(new Melysegi());
+            } while (beirt != "e");
+            
+
+
+
+        }
+        public static void Kiir(Kereso kereso)
+        {
+            foreach (Allapot allapot in kereso.Ut)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine(allapot.Karakterek[i]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Lépések száma: "+kereso.Ut.Count);
         }
     }
 }
