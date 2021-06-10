008c204daa674264106423b4a802dd6f8382b8be
Konya Donat <konya.donat@gmail.com>
2021-06-10 16:42:56 +0200

    Project created

---
+++ OA4R7U_2_25_Ketszemelyes/OA4R7U_2_25_Ketszemelyes/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OA4R7U_2_25_Ketszemelyes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
